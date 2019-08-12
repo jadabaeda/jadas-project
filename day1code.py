00ee1beaf852ce3e088b99676bff879f4ea8f7ec
jadabaeda <54039928+jadabaeda@users.noreply.github.com>
2019-08-12 15:15:39 -0400

    Add files via upload

---
+++ day1code.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+
+print("hello world")
+print("my name is jada")
+print("1226")
+
+5*2
+5/2
+5-2
+5**2
+8/9*3
+
+def add(a,b):
+    add = a+b
+    print(add)
+
+add(2,3)
+
+print("the sum of 23,70,and 97 is")
+def add(a,b,c):
+    add = a+b+c
+    print("the sum is ", add)
+    
+    
+add(23,70,97)
